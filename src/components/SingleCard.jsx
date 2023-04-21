51be3e9da6c39668897860a80c606ae2b7f500f8
Bkind254 <langatbrian02@gmail.com>
2023-04-21 16:43:41 +0300

    First commit

---
+++ src/components/SingleCard.jsx
@@ -0,0 +1,25 @@
+import "./SingleCard.css";
+
+const SingleCard = ({ card, handleChoice, flipped, disabled }) => {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
+
+  return (
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img className="front" src={card.src} alt="Card Front" />
+        <img
+          className="back"
+          src="/img/cover.png"
+          onClick={handleClick}
+          alt="Card Back"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SingleCard;
